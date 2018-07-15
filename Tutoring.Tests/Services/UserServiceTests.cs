5a2ca85bbd4bfac3c64b87e5df66e6fd5f59b74f
Mateusz Przeliorz <mateuszprzeliorz@outlook.com>
2018-07-15 16:29:51 +0200

    Fixed tests

--- Tutoring.Tests/Services/UserServiceTests.cs
+++ Tutoring.Tests/Services/UserServiceTests.cs
@@ -4,6 +4,7 @@ using Moq;
 using Tutoring.Core.Repositories;
 using Tutoring.Infrastructure.Services;
 using AutoMapper;
+using System;
 using Tutoring.Core.Domain;
 
 namespace Tutoring.Tests.Services
@@ -20,7 +21,7 @@ namespace Tutoring.Tests.Services
             encrypterMock.Setup(x => x.GetHash(It.IsAny<string>(), It.IsAny<string>())).Returns("salt");
 
             var userService = new UserService(userRepositoryMock.Object, mapperMock.Object, encrypterMock.Object);
-            await userService.RegisterAsync("user@email.com", "username1", "testpassword", "Wroclaw", "user");
+            await userService.RegisterAsync("user@email.com", "user", "secret", "Wroclaw");
 
             userRepositoryMock.Verify(x => x.AddAsync(It.IsAny<User>()), Times.Once);
         }
@@ -33,7 +34,7 @@ namespace Tutoring.Tests.Services
             var encrypterMock = new Mock<IEncrypter>();
             var userService = new UserService(userRepositoryMock.Object, mapperMock.Object, encrypterMock.Object);
             await userService.GetAsync("user1@email.com");
-            var user = new User("user1@email.com", "user1", "secret", "salt", "Wroclaw", "user");
+            var user = new User("user1@email.com", "user1", "secret", "salt", "Wroclaw");
 
             userRepositoryMock.Setup(x => x.GetAsync(It.IsAny<string>())).ReturnsAsync(user);
             userRepositoryMock.Verify(x => x.GetAsync(It.IsAny<string>()), Times.Once);
